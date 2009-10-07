8bc23d16020a03785831b3315e6c2fe5acc56a5f
venkatk <venkatk@45ff7e5c-2144-4d36-892c-87a539757531>
2009-10-07 05:43:37 +0000

    Silly Error, Untested code, this is fucked
    git-svn-id: http://grease.unfuddle.com/svn/grease_wtermappspot@11 45ff7e5c-2144-4d36-892c-87a539757531

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
+import string
 
 class MainPage(webapp.RequestHandler):
   def get(self):
@@ -16,12 +17,15 @@ class DemoPage( webapp.RequestHandler ):
     path = os.path.join(os.path.dirname(__file__), 'test.html')
     self.response.out.write(template.render(path, template_vars))
 
-application = webapp.WSGIApplication(
-                                     [
-                                       ('/', MainPage),
-                                       ('/demo', DemoPage)
-                                     ],
-                                     debug=True)
+class Strrev( webapp.RequestHandler ):
+  def get( self ):
+    s = str( self.request.get( 'tokens' ) )
+    arr = [ c for c in s ]
+    arr.reverse()
+    self.response.out.write( string.join( arr, '' ) )
+
+
+application = webapp.WSGIApplication([ ('/', MainPage), ('/demo', DemoPage), ('/strrev', Strrev) ], debug=True)
 
 def main():
   run_wsgi_app(application)
